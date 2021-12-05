9d66a57011800ba8237c38f6836f2d301190d2b8
lacie-life <00sao00ios00@gmail.com>
2021-12-05 16:47:55 +0700

    [Update]  add button

--- Flask-Image-Gallery/static/script.js
+++ Flask-Image-Gallery/static/script.js
@@ -1,10 +1,21 @@
 
 function myfunction(element) {
   console.log("1");
-  document.getElementById("img01").src = element.src;
+  document.getElementById("img00").src = element.src;
+
+  // document.getElementById("img01").src = "./results/" + path;
+  document.getElementById("modal00").style.display = "block";
+
+  document.getElementById('process').hidden = false;
+}
+
+function do_process() {
+  console.log("1");
+  document.getElementById("img01").src = document.getElementById("img00").src;
 
   document.getElementById("img02").src = element.src.replaceAll('cdn', 'results');
 
   // document.getElementById("img01").src = "./results/" + path;
   document.getElementById("modal01").style.display = "block";
 }
+
