4e4c82456d39ebe1a4119cdf8dc356eb1c840ba7
lacie-life <00sao00ios00@gmail.com>
2021-12-05 16:29:01 +0700

    [Update]  update results folder

--- Flask-Image-Gallery/static/script.js
+++ Flask-Image-Gallery/static/script.js
@@ -1,46 +1,10 @@
-// function clicked_img(img,fp){
-//           console.log(img.src);
 
-//           var top=document.getElementById('top')
-
-//           top.src = img.src;
-
-//           top.hidden=false;
-
-
-//           if (img.naturalWidth<screen.width*0.6 && img.naturalHeight<screen.height*0.6) {
-
-//             top.width=img.naturalWidth;
-//             top.height=img.naturalHeight;
-
-//           } else {
-
-//             top.width=screen.width*0.6;
-//             top.height=img.naturalHeight/img.naturalWidth*top.width;
-
-//           }
-
-//           document.getElementById('close').hidden = false;
-//  }
-
-
-// function do_close(){
-//   document.getElementById('top').hidden=true;
-//   document.getElementById('close').hidden=true;
-// }
-
-
-// document.getElementsByClassName("image").forEach((item) => {
-//   console.log("11");
-//   item.addEventListener("click", (event) => {
-//     alert("hehe");
-//     const image = event.target.getAttribute("data-src");
-//     event.target.setAttribute("src", image);
-//   });
-// });
 function myfunction(element) {
   console.log("1");
-  // document.getElementById("img01").src = element.src;
-  document.getElementById("img01").src = "./results/6173736574732f696d616765732f436c6f7365446574656374526573756c74302e6a7067";
+  document.getElementById("img01").src = element.src;
+
+  document.getElementById("img02").src = element.src.replaceAll('cdn', 'results');
+
+  // document.getElementById("img01").src = "./results/" + path;
   document.getElementById("modal01").style.display = "block";
 }
